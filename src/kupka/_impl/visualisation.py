cb910352b8e9b5a6dbfce7b2df615d9193233138
Norvan Sahiner <norvan@sahiner.com>
2025-01-26 19:20:32 +0100

    Refactor visualisation

--- src/kupka/_impl/visualisation.py
+++ src/kupka/_impl/visualisation.py
@@ -4,12 +4,21 @@ if TYPE_CHECKING:
     import pydot  # type: ignore
 
 
+def get_node_style(node: str) -> dict[str, str]:
+    return {
+        "label": node,
+        "color": "black",
+        #"style": "rounded",
+        "shape": "rect",
+    }
+
+
 def build_vis_graph(graph: dict[str, set[str]]) -> "pydot.Dot":
     import pydot  # type: ignore
 
     vis_graph = pydot.Dot("my_graph", graph_type="digraph", rankdir="LR")
     for node in graph.keys():
-        vis_graph.add_node(pydot.Node(node, label=f"{node}", color="black", style="rounded"))
+        vis_graph.add_node(pydot.Node(node, **get_node_style(node)))
     for node, dependencies in graph.items():
         for dependency in dependencies:
             vis_graph.add_edge(pydot.Edge(dependency, node, color="black"))
